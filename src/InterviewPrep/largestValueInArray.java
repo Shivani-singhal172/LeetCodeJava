16e602a64dd23d436c81f52958416bd262469f4a
Shivani Singhal <shivanisinghal@Shivanis-MacBook-Pro.local>
2026-06-25 22:09:59 +0530

    added code to find largest array using java

--- src/InterviewPrep/largestValueInArray.java
+++ src/InterviewPrep/largestValueInArray.java
@@ -3,31 +3,44 @@ package InterviewPrep;
 import java.util.Arrays;
 
 public class largestValueInArray {
-	
+
 	public static void main(String [] args) {
 		int[]  ar = {10,20,5,17,45,12};
-		largestValueInArray(ar);
+		largestValueInArrayUsingInBuiltMethods(ar);
+		findLargest(ar);
 	}
-	
-	public static void largestValueInArray(int[] a) {
-		
+
+	public static int largestValueInArrayUsingInBuiltMethods(int[] a) {
+
 		Arrays.sort(a);
 		System.out.println("The largest value is "+a[a.length-1]);
-//		for(int i = 0;i<a.length-1;i++) {
-//			System.out.println("The value of a[i]"+a[i] +" and a[i+1] " +a[i+1]);
-//			//sorting array
-//			if(a[i]>a[i+1]) {
-//				
-//				
-//				int b=a[i];
-//				a[i]=a[i+1];
-//				a[i+1]=b;
-//			}
-//			System.out.println(a[i]);
-//		}
-//		
-//		//Sorted array
+		return a[a.length-1];
+	}
+	
+	
+	public static int[] SortArray(int [] a) {
+		//Binary sorting
 		
+		for(int i = 0;i<a.length-1;i++) {
+			
+			for(int j = 0; j < a.length-i-1;j++) {
+				
+				if(a[j]>a[j+1]) {
+					int temp = a[j];
+					a[j]=a[j+1];
+					a[j+1]=temp;
+				}
+			}
+		}
+		
+		//sorted array is 
+		return a;
+	}
+	
+	public static int findLargest(int [] a) {
+		SortArray(a);
+		System.out.println("The largest value is "+a[a.length-1]);
+		return a[a.length-1];
 	}
 
 }
